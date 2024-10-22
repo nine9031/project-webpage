5803667a5b1a277bc30e1faf49359376c78b7df2
Nine <664259031@webmail.npru.ac.th>
2024-10-22 09:26:24 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+
+<script setup>
+import MyBarVue from './components/MyBar.vue';
+import MyCardVue from './components/MyCard.vue';
+import items from './attractions.json'
+</script>
+
+<template>
+  <header>
+    <MyBarVue />
+  </header>
+
+  <main>
+    <h1 style="paddingLeft: 10px">Attractions</h1>
+    <div className="grid-container">
+      <MyCardVue v-for="item in items" v-bind="item" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
+}
+@media (min-width: 576px) {
+  .grid-container { grid-template-columns: repeat(1, 1fr); }
+}
+@media (min-width: 992px) {
+  .grid-container { grid-template-columns: repeat(3, 1fr); }
+}
+</style>
---
+++ src/components/MyBar.vue
@@ -0,0 +1,34 @@
+<template lang="">
+  <div>
+    <ul>
+        <li><a href="default.asp">Home</a></li>
+        <li><a href="news.asp">News</a></li>
+        <li><a href="contact.asp">Contact</a></li>
+        <li><a href="about.asp">About</a></li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+li {
+  float: left;
+}
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+</style>
